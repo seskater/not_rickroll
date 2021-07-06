fc5d7e035c86b18f9ff59f8e94cbef5c7a17f6d1
vladbond00111 <vladbond00111@gmail.com>
2021-07-06 17:27:28 +0300

    first

---
+++ src/components/Goodbye.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="hello">
+   
+   <!-- <p class="seskater"> some viewers may find the content linked below inapropreate. please click "accept terms" to be able to wiev... </p>  
+  <iframe v-if="deth === true" id="ytplayer" type="text/html" width="1000" height="500"
+  src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1&origin=http://example.com"
+  frameborder="0"/> -->
+  <!-- <iframe v-if="deth === true" width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ" autoplay:1 title="YouTube video player" frameborder="0" allow="accelerometer; autoplay:1 ; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> -->
+    <v-btn
+      class="ma-2"
+      :loading="loading"
+      :disabled="loading"
+      color="secondary"
+      @click='die'
+    >
+     show more
+    </v-btn>
+    <div>
+      <v-card
+  elevation="21"
+  shaped
+></v-card>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'seskater',
+  props: {
+    msg: String
+  },
+   data: function () {
+     return {
+       deth: false,
+    
+     }
+  },
+  methods:{
+    die(){
+     this.deth= true
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+ .seskater {
+  display: flex;
+  margin: 0 10px;
+}
+
+</style>
---
+++ src/components/card.vue
@@ -0,0 +1,71 @@
+<template>
+  <v-card
+    class="mx-auto"
+    max-width="344"
+    outlined
+  >
+    <a class=   'sus' href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&feature=emb_logo&ab_channel=RickAstley">
+        <v-list-item three-line>
+        <v-list-item-content>
+            <div class="text-overline mb-4">
+            HEllO, {{ user.name }}
+            </div>
+            <v-list-item-title class="text-h5 mb-1">
+            watch now,{{user.name}}
+            </v-list-item-title>
+            <v-list-item-subtitle>Woman killed with Amongus Plushie </v-list-item-subtitle>
+        </v-list-item-content>
+
+        <v-list-item-avatar
+            tile
+            size="80"
+            color="grey"
+        >
+        <v-img
+            height="80"
+            src="../../public/amoguss.jpeg"
+            ></v-img>
+        </v-list-item-avatar>
+        </v-list-item>
+    </a>
+    
+  </v-card>
+ 
+</template>
+
+<script>
+export default {
+  name: 'seskater',
+  props: {
+    msg: String
+  },
+   data: function () {
+     return {
+       title: 'seskater',
+       number: 69,
+       user: {
+         name: 'Seskater',
+         link: 'https://youtu.be/dQw4w9WgXcQ',
+         sus: 'Y',
+         img:'./public/amoguss.jpeg'
+       }
+     }
+  },
+  methods:{
+    die(){
+     this.deth= true
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+ .seskater {
+  display: flex;
+  margin: 0 10px;
+}
+.sus {
+    text-decoration: none;
+ }
+</style>
